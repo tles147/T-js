762473ce0e2c41f978ea53d1c7df7b8574825823
tles-js <tless1477@gmail.com>
2024-11-19 20:26:23 +0700

    first commit

---
+++ src/assets/mock/mock.js
@@ -0,0 +1,17 @@
+import image1 from "C:\Users\TLES\Downloads\surprise-birthday-basic-master\surprise-birthday-basic-master\src\assets\images\img.jpg";
+import gift1 from "C:\Users\TLES\Downloads\surprise-birthday-basic-master\surprise-birthday-basic-master\src\assets\images\gifts\imm.jpg";
+
+export const _albums = image1;
+
+export const _giftAlbums = gift1;
+
+export const _messages = [
+    "ลองทำดูเล่นๆให้อ่ะ อาจจะไม่สวยเท่าไหร่เพราะทำไม่ค่อยเก่ง55554564545",
+    "แต่รักนะะ",
+];
+
+export const _birthdayMessages = [
+    "ครบรอบ2เดือนแระ",
+    "ไวจัง แต่ไม่มีไรให้หนุเลยยย",
+    "ไม่ค่อยได้ถ่ายรูปกันเลยเอารูปนี้ไปก่อนน",
+];
